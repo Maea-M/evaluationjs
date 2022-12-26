e86d2da05e8749c26c03f2d34ed029aa284bbc16
Morgane Mahé <maeamahe@gmail.com>
2022-12-26 11:17:58 +0100

    write idea of algo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("coucou!");
+
+/* 
+1/ lorsqu'on clique sur le boutton NewGame, les scores sont remis à 0
+
+2/Lorsqu'on clique sur le boutton Hold :
+- les rounds du player sont envoyés dans le global
+- c'est au tour de l'autre joueur de lancer
+
+3/Lorsqu'on clique sur ROllDice:
+- un nombre aléatoire est généré par le dé
+- le dé doit changer de face
+
+4/Lorsque la valeur du dé vaut 1:
+- le score round revient à zéro
+- c'est au tour de l'autre joueur de lancer
+
+5/Lorsqu'un joueur arrive à 100:
+- la partie est gagnée
+- on ne peut plus continuer à jouer
