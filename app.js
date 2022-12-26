271a586de924c8bf76cf91035da74539d982bc9e
Morgane Mahé <maeamahe@gmail.com>
2022-12-26 12:19:59 +0100

    add number random

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-let globalPlayerOne = 0;
-let globalPlayerTwo = 0;
+let globalPlayer, roundScore, activePlayer,  
 
 /* 
 1/ lorsqu'on clique sur le boutton NewGame, les scores sont remis à 0
 */
-let newGame = document.getElementById("newGame").addEventListener("click", () => {
-    globalPlayerOne = 0;
-    globalPlayerTwo = 0;
+let newGame = document.getElementById("newGame").addEventListener("click", ()=>{
+    globalPlayer = 0;
 }
 );
 
-/*2/Lorsqu'on clique sur le boutton Hold :
+/*2/Lorsqu'on clique sur ROllDice:
+- un nombre aléatoire est généré par le dé
+- le dé doit changer de face*/
+let rollDice = document.getElementById("rollDice").addEventListener("click", ()=>{
+    let dice = Math.floor(Math.random()* 6 ) + 1 ;
+    roundScore += dice;
+};
+
+/*3/Lorsqu'on clique sur le boutton Hold :
 - les rounds du player sont envoyés dans le global
 - c'est au tour de l'autre joueur de lancer
+*/
+let hold = document.getElementById("hold").addEventListener("click", ()=>{
+    globalPlayer += roundScore;
+}
+);
 
-3/Lorsqu'on clique sur ROllDice:
-- un nombre aléatoire est généré par le dé
-- le dé doit changer de face
-
+/*
 4/Lorsque la valeur du dé vaut 1:
 - le score round revient à zéro
 - c'est au tour de l'autre joueur de lancer
