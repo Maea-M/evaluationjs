fa1ea906f222bfe4cbcd932e0e16d98b61be7cc9
Morgane Mahé <maeamahe@gmail.com>
2022-12-26 18:44:48 +0100

    add imagedice but not works

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
 let globalPlayer, roundScore, activePlayer  
 const diceGenerate = document.getElementById("diceGenerate");
+let diceArray = ['face1.png', 'face2.png', 'face3.png', 'face4.png', 'face5.png', 'face6.png'];
+
 /* 
-1/ lorsqu'on clique sur le boutton NewGame, les scores sont remis à 0
+1/ lorsqu'on clique sur le boutton NewGame, les scores sont remis à 0, player1 est actif
 */
+function beginGame() {
+    globalPlayer = [0,0];
+    roundScore = 0;
+    activePlayer = 'player1';
+    launchGame = true;
+}
 const newGame = document.getElementById("newGame").addEventListener("click", ()=>{
-    console.log(globalPlayer = 0);
+    beginGame()
 }
 );
 
-/*function changer de joueur : le roundScore revient à 0, le joueur inactif veient actif*/
+
+/*function changer de joueur : le roundScore revient à 0, 
+le joueur inactif veient actif
+le currentScore revient à 0
+*/
 function nextPlayer(){
     roundScore = 0;
+
     if (activePlayer === 'player1') {
         activePlayer = 'player2';
     }else activePlayer = 'player1';
+
+    document.getElementById("currentScoreOne").textContent = 0;
+    document.getElementById("currentScoreTwo").textContent = 0;
+    document.querySelector(".player-One-Container").classList.toggle("active");
+    document.querySelector(".player-Two-Container").classList.toggle("active");
+    document.querySelector("#diceImg").style.display = "block";
 }
-/*2/Lorsqu'on clique sur ROllDice:
-- un nombre aléatoire est généré par le dé
-- le dé doit changer de face*/
-/*
-4/Lorsque la valeur du dé vaut 1:
-- le score round revient à zéro
-- c'est au tour de l'autre joueur de lancer*/
+
+/*2/Lorsqu'on clique sur ROllDice*/
 const rollDice = document.getElementById("rollDice").addEventListener("click", ()=>{
+    /*un nombre aléatoire est généré par le dé*/
     let dice = Math.floor(Math.random()* 6 ) + 1 ;
     console.log(dice);
-    dice.innerHTML = diceGenerate;
+
+    /*- le dé doit changer de face*/
+    let image = document.getElementById("diceImg")
+    image.scr = `images/face${dice}.png`;
+
+    /*Lorsque la valeur du dé vaut 1:
+    - le score round revient à zéro
+    - c'est au tour de l'autre joueur de lancer*/
     if (dice !== 1){
         roundScore += dice;
     } else{
@@ -34,12 +56,12 @@ const rollDice = document.getElementById("rollDice").addEventListener("click", (
 }
 );
 
-/*3/Lorsqu'on clique sur le boutton Hold :
-- les rounds du player sont envoyés dans le global
-- c'est au tour de l'autre joueur de lancer
-*/
+/*3/Lorsqu'on clique sur le boutton Hold*/
 const hold = document.getElementById("hold").addEventListener("click", ()=>{
+    /*les rounds du player sont envoyés dans le global*/
     globalPlayer += roundScore;
+
+    /*c'est au tour de l'autre joueur de lancer*/
     nextPlayer();
 }
 );
