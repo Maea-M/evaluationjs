c9aea1c1e7fc7f0f5e0da40a657024ec52802934
Morgane Mahé <maeamahe@gmail.com>
2022-12-28 18:49:53 +0100

    worth than before : check if error syntax

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-let globalPlayer, roundScore, activePlayer, launchGame
+/*
+GlobalPlayer = tableau pour récupérer les scores des 2 joueurs, 
+roundScore = le score par set,
+activePlayer = player 1 ou 2,
+launchGame =  vrai si lancé
+*/
+let globalPlayer, roundScore, activePlayer, launchGame;
+
+/*faire des const pour bouttons et dé*/
+const dice = document.querySelector('.diceImg');
+const newGame = document.querySelector('#newGame');
+const rollDice = document.querySelector('#rollDice');
+const hold = document.querySelector('#hold');
+
+/* faire comme une card memory pour les dés?*/
+rollDice.addEventListener('click',()=>{
+    if (diceImg.classList.contains("hidden")){
+        diceImg.classList.remove("hidden")
+    } else {
+        diceImg.classList.add("hidden")
+    }
+})
 
 /* 
 1/ lorsqu'on clique sur le boutton NewGame, les scores sont remis à 0, player1 est actif
@@ -9,10 +30,6 @@ function beginGame() {
     activePlayer = '1';
     launchGame = true;
 }
-const newGame = document.getElementById("newGame").addEventListener("click", ()=>{
-    beginGame()
-}
-);
 
 
 /*function changer de joueur : le roundScore revient à 0, 
@@ -24,63 +41,61 @@ function nextPlayer(){
 
     if (activePlayer === '1') {
         activePlayer = '2';
-    }else activePlayer = '1';
+    } else activePlayer = '1';
 
     document.getElementById("current-1").textContent = 0;
     document.getElementById("current-2").textContent = 0;
     document.querySelector(".player-One-Container").classList.toggle("active");
     document.querySelector(".player-Two-Container").classList.toggle("active");
-    document.querySelector("#diceImg").style.display = "block";
 }
 
 /*2/Lorsqu'on clique sur ROllDice*/
-document.getElementById("rollDice").addEventListener("click", ()=>{
-    if(launchGame) {
+rollDice.addEventListener('click', ()=>{
+    if (launchGame) {
         /*un nombre aléatoire est généré par le dé*/
         let dice = Math.floor(Math.random()* 6 ) + 1 ;
         console.log(dice);
-
+    
         /*- le dé doit changer de face*/
-        /*let image = document.querySelector("diceImg");
-        image.style.display = "block";
-        image.src = `images/face-${dice}.png`;*/
-
+        let image = document.querySelector("diceImg");
+        image.remove('hidden');
+        image.src = `face-${dice}.png`;
+    
         /*Lorsque la valeur du dé vaut 1:
         - le score round revient à zéro*/
         if (dice !== 1){
         roundScore += dice;
         document.getElementById(`current-${activePlayer}`).textContent = roundScore;
         console.log(roundScore)
-        /*        - c'est au tour de l'autre joueur de lancer*/
-
+        /*- c'est au tour de l'autre joueur de lancer*/
         } else{
         nextPlayer();
         }
     }
-}
-);
-
-/*3/Lorsqu'on clique sur le boutton Hold*/
-document.getElementById("hold").addEventListener("click", ()=>{
-    if (launchGame) {
-        /*les rounds du player sont envoyés dans le global*/
-        globalPlayer [activePlayer] += roundScore;
-        document.querySelector("#score-" + activePlayer).textContent = globalPlayer[activePlayer];
-        console.log(globalPlayer)
-    }
+});
     
-    /*c'est au tour de l'autre joueur de lancer*/
-    nextPlayer();
-}
+    /*3/Lorsqu'on clique sur le boutton Hold*/
+    hold.addEventListener("click", ()=>{
+        if (launchGame) {
+            /*les rounds du player sont envoyés dans le global*/
+            globalPlayer [activePlayer] += roundScore;
+            document.querySelector("#score-" + activePlayer).textContent = globalPlayer[activePlayer];
+            console.log(globalPlayer)
+        }
+        
+        /*c'est au tour de l'autre joueur de lancer*/
+        nextPlayer();
+    }
 );
 
 /*5/Lorsqu'un joueur arrive à 100:
 - la partie est gagnée
 - on ne peut plus continuer à jouer
 */
+
 if (globalPlayer[activePlayer] >= 100) {
-    alert('Vous avez gagné')
-    launchGame = false;
+    launchGame = false
+    dice.classList.add('hidden');
     } else {
-    endGame();
+    alert ('Une erreur s\'est produite');
     };
