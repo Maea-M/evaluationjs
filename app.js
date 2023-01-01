9c2017accbbd0c01111fcc12adf56190d5d3e64e
Morgane Mahé <maeamahe@gmail.com>
2023-01-01 09:42:05 +0100

    fix end game

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 /*
-GlobalPlayer = tableau pour récupérer les scores des 2 joueurs, 
+GlobalPlayer = array to get scores of 2 players, 
 id = globalScore-1 globalScore-2
-roundScore = le score par set,
-id= current-1 et current-2
+roundScore =  score by set,
+id= current-1 and current-2
 activePlayer = player 1 ou 2,
-id= player-1 et player-2
-launchGame =  vrai si lancé
+id= player-1 and player-2
+launchGame =  true if begin
 */
 let globalPlayer, roundScore, activePlayer, launchGame;
 
-/*faire des const pour bouttons et dé
-id dé =  diceImg
-id boutton nouvelle partie = newGame
-id boutton lancer le dé = rollDice
-id boutton tenir = hold
+/*make  const for buttons and dice
+id dice =  diceImg
+id button new game = newGame
+id button roll the dice = rollDice
+id button hold = hold
 */
 const dice = document.querySelector('#diceImg');
 const newGame = document.querySelector('#newGame');
@@ -21,7 +21,7 @@ const rollDice = document.querySelector('#rollDice');
 const hold = document.querySelector('#hold');
 
 /* 
-1/ lorsqu'on clique sur le boutton NewGame, les scores sont remis à 0, player1 est actif
+1/ when we click on the button NewGame, scores goes to 0, player1 is active
 */
 function beginGame() {
     globalPlayer = [0, 0];
@@ -31,9 +31,9 @@ function beginGame() {
 }
 beginGame()
 
-/*function changer de joueur : le roundScore revient à 0, 
-le joueur inactif veient actif
-le currentScore revient à 0
+/*function changer player :  roundScore goes to 0, 
+player became inactive 
+currentScore goes to 0
 */
 function nextPlayer(){
     roundScore = 0;
@@ -48,53 +48,45 @@ function nextPlayer(){
     document.querySelector(".player-Two-Container").classList.toggle("active");
 }
 
-/*2/Lorsqu'on clique sur ROllDice*/
+/*2/When we click on the button ROllDice*/
 rollDice.addEventListener('click', ()=>{
     if (launchGame) {
-        /*un nombre aléatoire est généré par le dé*/
+        /*number random is generate by the dice*/
         let dice = Math.floor(Math.random()* 6 ) + 1 ;
     
-        /*- le dé doit changer de face*/
+        /*- dice must change face*/
         let image = document.querySelector("#diceImg");
         image.src = `images/face-${dice}.png`;
     
         /*Lorsque la valeur du dé vaut 1:
-        - le score round revient à zéro*/
+        - le score round goes to zero*/
         if (dice !== 1){
         roundScore += dice;
         document.getElementById(`current-${activePlayer}`).textContent = roundScore;
         console.log(roundScore)
-        /*- c'est au tour de l'autre joueur de lancer*/
+        /*- next player*/
         } else{
         nextPlayer();
         }
     }
 });
     
-    /*3/Lorsqu'on clique sur le boutton Hold*/
-hold.addEventListener("click", ()=>{
+    /*3/When we click on the button Hold*/
+
+hold.addEventListener('click', () =>{
     if (launchGame) {
-        /*les rounds du player sont envoyés dans le global*/
-        globalPlayer [activePlayer] += roundScore;
+    /*- Add current score to global player's score*/
+        globalPlayer [activePlayer] += roundScore; 
         document.getElementById(`globalScore-${activePlayer}`).textContent =
         globalPlayer [activePlayer];
-        console.log(globalPlayer)
-        nextPlayer()
-    } else { 
-        
-    };
+
+    /*- Check if player's score is >= 100 */
+    if (globalPlayer [activePlayer] >= 100) {
+        launchGame = false;
+        diceImg.classList.add('hidden');
+        alert('Vous avez gagné!')
+    } else {
+        nextPlayer();
     }
-);
-
-/*5/Lorsqu'un joueur arrive à 100:
-- la partie est gagnée
-- on ne peut plus continuer à jouer
-*/
-
-if (globalPlayer[activePlayer] >= 100) {
-    alert ('vous avez gagné!')
-    launchGame = false
-    dice.classList.add('hidden');
-} else {
-    nextPlayer();
-}
+    }
+});
